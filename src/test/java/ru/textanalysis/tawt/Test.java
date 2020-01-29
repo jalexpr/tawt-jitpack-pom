af176469ec89a84a2c5a49324302f89046334b29
APorechny <aporechny@mfd.ru>
2020-01-29 10:44:41 +0300

    up version tawt

--- src/test/java/ru/textanalysis/tawt/Test.java
+++ src/test/java/ru/textanalysis/tawt/Test.java
@@ -1,3 +1,6 @@
+package ru.textanalysis.tawt;
+
+import ru.textanalysis.tawt.ms.external.sp.BearingPhraseExt;
 import ru.textanalysis.tawt.ms.internal.sp.BearingPhraseSP;
 import ru.textanalysis.tawt.sp.api.SyntaxParser;
 
@@ -9,6 +12,11 @@ public class Test {
         sp.init();
         List<BearingPhraseSP> phrase
                 = sp.getTreeSentence("Стало ясно, что будет с российской валютой.");
+        phrase.forEach(System.out::println);
+        List<BearingPhraseExt> phraseExts
+                = sp.getTreeSentenceWithoutAmbiguity("Стало ясно, что будет с российской валютой. " +
+                "Мама мыла рамую.");
+
         phrase.forEach(System.out::println);
     }
 }
